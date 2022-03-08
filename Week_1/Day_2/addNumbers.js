c92c29e3b64134d3811f8eceaf8e0e009676f77a
Lisa Meyer <lisamermey@gmail.com>
2022-03-08 12:23:17 -0500

    refactor to a function

--- Week_1/Day_2/addNumbers.js
+++ Week_1/Day_2/addNumbers.js
@@ -1,23 +1,21 @@
 // We are tasted to create a program that takes in any number of command line arguments which will be numbers
 // Add the numbers together and return a sum of all the numbers
 
-// How do I get command line arguments
-// Once command line arguments have come back...
-// How do I add numbers together?
-// How do I add multiple Numbers together (where size of elements is different)?
 
-// retrieve command line arguments and store Numbers in Array numbers
-const args = process.argv;
-const numbers = args.slice(2);
-console.log(numbers);
+const addNumbers = () => {
+  // retrieve command line arguments and store Numbers in Array numbers
+  const args = process.argv;
+  const numbers = args.slice(2);
 
-// initialize an accumulator
-let sum = 0;
-
-// iterate over numbers Array
-for (let i = 0; i < numbers.length; i++) {
-  console.log("i ",i);
-  console.log("numbers[i] ",numbers[i]);
-  sum += Number(numbers[i]);
+  // initialize an accumulator
+  let sum = 0;
+  
+  // iterate over numbers Array
+  for (let i = 0; i < numbers.length; i++) {
+    sum += Number(numbers[i]);
+  }
+  console.log(sum);
 }
-console.log(sum);
+
+// call the function
+addNumbers();
