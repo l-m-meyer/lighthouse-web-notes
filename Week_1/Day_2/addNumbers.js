6bcffec9f0a66730ffb17098ab5167e3fd2cc415
Lisa Meyer <lisamermey@gmail.com>
2022-03-08 12:26:19 -0500

    refactor function to take argument

--- Week_1/Day_2/addNumbers.js
+++ Week_1/Day_2/addNumbers.js
@@ -1,21 +1,21 @@
 // We are tasted to create a program that takes in any number of command line arguments which will be numbers
 // Add the numbers together and return a sum of all the numbers
 
+// retrieve command line arguments and store Numbers in Array numbers
+const args = process.argv;
+const numbers = args.slice(2);
 
-const addNumbers = () => {
-  // retrieve command line arguments and store Numbers in Array numbers
-  const args = process.argv;
-  const numbers = args.slice(2);
-
+const addNumbers = (arr) => {
   // initialize an accumulator
   let sum = 0;
   
   // iterate over numbers Array
-  for (let i = 0; i < numbers.length; i++) {
-    sum += Number(numbers[i]);
+  for (let i = 0; i < arr.length; i++) {
+    sum += Number(arr[i]);
   }
   console.log(sum);
 }
 
 // call the function
-addNumbers();
+addNumbers(numbers);
+addNumbers([4, 4, 4, 4]);
