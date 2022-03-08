ba0f7a7cf019ac1da30452e4bbc511f27c7e42b8
Lisa Meyer <lisamermey@gmail.com>
2022-03-08 11:56:50 -0500

    Established the goal of program and start to add pseudocode

---
+++ Week_1/Day_2/addNumbers.js
@@ -0,0 +1,6 @@
+// We are tasted to create a program that takes in any number of command line arguments which will be numbers
+// Add the numbers together and return a sum of all the numbers
+
+// How do I get command line arguments
+// Once command line arguments have come back...
+// How do I add numbers together?
