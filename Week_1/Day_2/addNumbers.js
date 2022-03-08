b4e6d45c5b01219ec9fdf9f1cdbb3aff28894b77
Lisa Meyer <lisamermey@gmail.com>
2022-03-08 12:05:36 -0500

    retrieve arguments from the command line

--- Week_1/Day_2/addNumbers.js
+++ Week_1/Day_2/addNumbers.js
@@ -5,3 +5,8 @@
 // Once command line arguments have come back...
 // How do I add numbers together?
 // How do I add multiple Numbers together (where size of elements is different)?
+
+// retrieve command line arguments and store Numbers in Array numbers
+const args = process.argv;
+const numbers = args.slice(2);
+console.log(numbers);
